02bee05865abe74e05be7de439d183eb5413ae98
Yankey N'wollé Ange Christian <168791054+Yankeyange@users.noreply.github.com>
2024-11-14 01:02:39 +0000

    Add files via upload

--- auctions/models.py
+++ auctions/models.py
@@ -2,5 +2,33 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+# création de touts mes modèles nécessaires
+
+# modèles Utilisateurs déjà intégré de Django
 class User(AbstractUser):
     pass
+
+# modèles pour les enchères(Auctions)
+
+class Auctions(models.Model):
+    name = models.CharField(max_length=200)
+    title = models.TextField(max_length=100)
+    image = models.ImageField(upload_to="images/", height_field=None, width_field=None, max_length=100)
+    description = models.TextField(max_length=100)
+    price = models.IntegerField()
+    # la date à laquelle l'utilisateur va créer sa vente 
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+# modèles pour les offres(bids)
+
+class Bids(models.Model):
+    price = models.IntegerField()
+
+
+# modèles pour les commentaires
+
+class Comment(models.Model):
+    text = models.ForeignKey(Auctions, on_delete=models.CASCADE)
