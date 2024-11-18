d2555aee7e2d7b5812aeabcd05c77bf3e850c4c6
Yankey N'wollé Ange Christian <168791054+Yankeyange@users.noreply.github.com>
2024-11-18 17:02:20 +0000

    Add files via upload

--- commerce/urls.py
+++ commerce/urls.py
@@ -15,9 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-
+from django.conf import settings
+from django.conf.urls.static import static
+from auctions.views import CreateListing, Watchlist, Categories
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("CreateListing",CreateListing, name="CreateListing"),
+    path("Watchlist",Watchlist, name="Watchlist"),
+    path("Categories",Categories, name="Categories"),
     path("", include("auctions.urls"))
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
