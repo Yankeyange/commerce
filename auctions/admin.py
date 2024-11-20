45c0b1d635482b15ba68a5aaa1b19b62405827ba
Yankey N'wollé Ange Christian <168791054+Yankeyange@users.noreply.github.com>
2024-11-20 12:35:02 +0000

    Add files via upload

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Auctions,Details
 
-# Register your models here.
+class AdminAuctions(admin.ModelAdmin):
+  list_display = ('name', "title", "image","description","date","price")
+
+admin.site.register(Auctions, AdminAuctions)
+admin.site.register(Details)
+
